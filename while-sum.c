a388bd7b931c1ec9b87fac72cbfe7a33af4efed8
Moroshima <2524332942aliez@gmail.com>
2020-10-14 20:57:57 +0800

    Update

---
+++ while-sum.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int i,sum=0;
+    i=1;
+    while(i<=100)  //循环条件应该是什么呢？
+    {
+        sum=sum+i;
+        i=i+1;//这里是不是应该改变循环变量的值
+    }
+    printf("100以内所有整数之和为：%d\n", sum);
+    return 0;
+}
